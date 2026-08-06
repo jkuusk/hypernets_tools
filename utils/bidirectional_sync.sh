21c4abe7fd820fe009d4d55a10073a1d808046ca
Joel Kuusk <joelk@ut.ee>
2026-08-06 15:30:52 +0300

    inaccessible secondary server blocks upload to primary server

--- utils/bidirectional_sync.sh
+++ utils/bidirectional_sync.sh
@@ -18,55 +18,55 @@ set -o nounset                              # Treat unset variables as an error
 set -euo pipefail                           # Bash Strict Mode	
 
 bidirectional_sync(){
+	local localPath="$1"
+	local remoteAccess="$2"
+	local remotePath="$3"
+	local sshPort="$4"
+	local server="$5"
 
-	localPath="$1"
-	remoteAccess="$2"
-	remotePath="$3"
-	sshPort="$4"
-
-	echo "[INFO]  Sync files : $remoteAccess:$remotePath"
-	echo "[INFO]          <->  $localPath"
+	echo "[INFO]  ($server server) Sync files : $remoteAccess:$remotePath"
+	echo "[INFO]  ($server server)        <->  $localPath"
 
 	set +e  # Temporary allow error in script
-	remoteDate=$(ssh -p "$sshPort" -T "$remoteAccess" \
+	local remoteDate=$(ssh -p "$sshPort" -T "$remoteAccess" \
 		"stat -c %y $remotePath 2> /dev/null")
 
-	retcode="$?"
+	local retcode="$?"
 
 	if [[ "$retcode" -eq 1 ]]; then
-		echo "[INFO]  $0 : Remote file does not exist, uploading now"
+		echo "[INFO]  ($server server) $0 : Remote file does not exist, uploading now"
 		scp -p -P "$sshPort" "$localPath" "$remoteAccess:$remotePath"
 		return $?
 	elif [[ "$retcode" -eq 255 || "$remoteDate" == "" ]]; then
-		echo "[ERROR]  Failed to retreive remote file modification time"
+		echo "[ERROR]  ($server server) Failed to retreive remote file modification time"
 		return -1
 	fi
 
-	localDate=$(stat -c %y "$localPath" 2> /dev/null)
+	local localDate=$(stat -c %y "$localPath" 2> /dev/null)
 	if [[ "$?" -eq 1 ]]; then
-		echo "[INFO]  $0 : Local file does not exist, downloading now"
+		echo "[INFO]  ($server server) $0 : Local file does not exist, downloading now"
 		scp -p -P "$sshPort" "$remoteAccess:$remotePath" "$localPath"
 		return $?
 	fi
 	set -e  # Back to strict mode
 
 	# Conversion in integer
-	remoteTimeStamp=$(date -d "$remoteDate" +%s)
-	localTimeStamp=$(date -d "$localDate" +%s)
+	local remoteTimeStamp=$(date -d "$remoteDate" +%s)
+	local localTimeStamp=$(date -d "$localDate" +%s)
 
 	# Both files exists, compare of datetimes and sync
 	if [ "$remoteTimeStamp" -gt "$localTimeStamp" ] ; then
-		echo "[INFO]  Local date: $localDate"
-		echo "[INFO]  Remote date: $remoteDate"
-		echo "[INFO]  Sync from remote to local"
+		echo "[INFO]  ($server server) Local date: $localDate"
+		echo "[INFO]  ($server server) Remote date: $remoteDate"
+		echo "[INFO]  ($server server) Sync from remote to local"
 		rsync -e "ssh -p $sshPort" -vt "$remoteAccess:$remotePath" "$localPath"
 	elif [ "$remoteTimeStamp" -lt "$localTimeStamp" ] ; then
-		echo "[INFO]  Local date: $localDate"
-		echo "[INFO]  Remote date: $remoteDate"
-		echo "[INFO]  Sync from local to remote"
+		echo "[INFO]  ($server server) Local date: $localDate"
+		echo "[INFO]  ($server server) Remote date: $remoteDate"
+		echo "[INFO]  ($server server) Sync from local to remote"
 		rsync -e "ssh -p $sshPort" -vt "$localPath" "$remoteAccess:$remotePath"
 	else
-		echo "[INFO]  Files are synchronized."
+		echo "[INFO]  ($server server) Files are synchronized."
 	fi
 	return $?
 }
